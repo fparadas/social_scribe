4057f8e50a5d0294d449907a3a3907149098b156
fparadas <felipegparadas@gmail.com>
2025-05-24 10:19:48 -0300

    first commit

---
+++ lib/social_scribe/application.ex
@@ -0,0 +1,36 @@
+defmodule SocialScribe.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      SocialScribeWeb.Telemetry,
+      SocialScribe.Repo,
+      {DNSCluster, query: Application.get_env(:social_scribe, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: SocialScribe.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: SocialScribe.Finch},
+      # Start a worker by calling: SocialScribe.Worker.start_link(arg)
+      # {SocialScribe.Worker, arg},
+      # Start to serve requests, typically the last entry
+      SocialScribeWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: SocialScribe.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    SocialScribeWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ lib/social_scribe_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule SocialScribeWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use SocialScribeWeb, :html
+
+  embed_templates "page_html/*"
+end
