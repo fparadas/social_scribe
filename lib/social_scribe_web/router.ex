9a01457707168479d6f2a9e3d52d72027ee224ec
fparadas <felipegparadas@gmail.com>
2025-05-24 13:21:42 -0300

    cleaning

--- lib/social_scribe_web/router.ex
+++ lib/social_scribe_web/router.ex
@@ -48,11 +48,6 @@ defmodule SocialScribeWeb.Router do
   scope "/", SocialScribeWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    live_session :redirect_if_user_is_authenticated,
-      on_mount: [{SocialScribeWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/log_in", UserLoginLive, :new
-    end
-
     post "/users/log_in", UserSessionController, :create
   end
 
