4f1e78e1d9ef3fb79c490530c840be93f567b337
fparadas <felipegparadas@gmail.com>
2025-05-24 23:58:25 -0300

    cleaning and minor fixes

--- lib/social_scribe_web/live/meeting_live/show.ex
+++ lib/social_scribe_web/live/meeting_live/show.ex
@@ -5,44 +5,35 @@ defmodule SocialScribeWeb.MeetingLive.Show do
 
   @impl true
   def mount(%{"id" => meeting_id}, _session, socket) do
-    # Fetch the meeting with all its details preloaded
-    # The get_meeting_with_details! function should preload:
-    # :calendar_event, :recall_bot, :meeting_transcript, :meeting_participants
     meeting = Meetings.get_meeting_with_details!(meeting_id)
 
-    # Basic security check: ensure the meeting belongs to the current user
-    # You might want to make this more robust depending on your data model
-    # For example, if Meetings.get_meeting_with_details! can be scoped by user.
-    # current_user_id = socket.assigns.current_user.id
-    # if meeting.calendar_event.user_id != current_user_id do
-    #   socket =
-    #     socket
-    #     |> put_flash(:error, "You do not have permission to view this meeting.")
-    #     |> redirect(to: ~p"/meetings")
-    #   {:error, socket} # This would stop the mount
-    # else
-    #   ... proceed
-    # end
-    # For now, assuming get_meeting_with_details! is safe or you'll add scoping
+    if meeting.calendar_event.user_id != socket.assigns.current_user.id do
+      socket =
+        socket
+        |> put_flash(:error, "You do not have permission to view this meeting.")
+        |> redirect(to: ~p"/dashboard/meetings")
 
-    socket =
-      socket
-      |> assign(:page_title, "Meeting Details: #{meeting.title}")
-      |> assign(:meeting, meeting)
-      |> assign(
-        :ai_follow_up_email,
-        "AI-generated follow-up email will appear here once generated."
-      )
-      |> assign(:ai_social_posts_list, [
-        %{platform: "LinkedIn", content: "LinkedIn post draft from automation will be here."},
-        %{platform: "Facebook", content: "Facebook post draft from automation will be here."}
-      ])
-      |> assign(
-        :ai_draft_social_post,
-        "A manually generated AI draft social media post will appear here."
-      )
+      {:error, socket}
+    else
+      socket =
+        socket
+        |> assign(:page_title, "Meeting Details: #{meeting.title}")
+        |> assign(:meeting, meeting)
+        |> assign(
+          :ai_follow_up_email,
+          "AI-generated follow-up email will appear here once generated."
+        )
+        |> assign(:ai_social_posts_list, [
+          %{platform: "LinkedIn", content: "LinkedIn post draft from automation will be here."},
+          %{platform: "Facebook", content: "Facebook post draft from automation will be here."}
+        ])
+        |> assign(
+          :ai_draft_social_post,
+          "A manually generated AI draft social media post will appear here."
+        )
 
-    {:ok, socket}
+      {:ok, socket}
+    end
   end
 
   defp format_duration(nil), do: "N/A"
